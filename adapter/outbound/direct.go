2aafc4c2d418aee2cf71c226f31eabd498bb4e41
xfz347 <245970791@qq.com>
2026-09-23 11:28:00 +0800

    修复代理挂了直连也挂的问题
    Signed-off-by: xfz347 <245970791@qq.com>

--- adapter/outbound/direct.go
+++ adapter/outbound/direct.go
@@ -26,9 +26,21 @@ func (d *Direct) DialContext(ctx context.Context, metadata *C.Metadata) (C.Conn,
 		return nil, err
 	}
 	opts := d.DialOptions()
-	opts = append(opts, dialer.WithResolver(resolver.DirectHostResolver))
+	directResolver := resolver.DirectHostResolver
+	opts = append(opts, dialer.WithResolver(directResolver))
 	c, err := dialer.DialContext(ctx, "tcp", metadata.RemoteAddress(), opts...)
 	if err != nil {
+		if directResolver != nil && directResolver == resolver.DefaultResolver && metadata.Host != "" {
+			ip, resolveErr := resolver.SystemResolver.LookupIP(ctx, metadata.Host)
+			if resolveErr == nil && len(ip) > 0 {
+				metadata.DstIP = ip[0]
+				opts2 := d.DialOptions()
+				c2, dialErr := dialer.DialContext(ctx, "tcp", metadata.RemoteAddress(), opts2...)
+				if dialErr == nil {
+					return d.loopBack.NewConn(NewConn(c2, d)), nil
+				}
+			}
+		}
 		return nil, err
 	}
 	return d.loopBack.NewConn(NewConn(c, d)), nil
@@ -53,6 +65,12 @@ func (d *Direct) ResolveUDP(ctx context.Context, metadata *C.Metadata) error {
 	if (!metadata.Resolved() || resolver.DirectHostResolver != resolver.DefaultResolver) && metadata.Host != "" {
 		ip, err := resolveIPWithResolver(ctx, metadata.Host, d.prefer, resolver.DirectHostResolver)
 		if err != nil {
+			if resolver.DirectHostResolver == resolver.DefaultResolver {
+				if sysIP, sysErr := resolver.SystemResolver.LookupIP(ctx, metadata.Host); sysErr == nil && len(sysIP) > 0 {
+					metadata.DstIP = sysIP[0]
+					return nil
+				}
+			}
 			return fmt.Errorf("can't resolve ip: %w", err)
 		}
 		metadata.DstIP = ip
